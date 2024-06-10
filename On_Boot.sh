1367b79cc3d02be852de84e3f8e2bf45b72ee394
assume-breach <76174163+assume-breach@users.noreply.github.com>
2024-06-10 11:12:36 -0400

    Create On_Boot.sh

---
+++ On_Boot.sh
@@ -0,0 +1,56 @@
+# Make it executable
+chmod +x /opt/IT_For_The_End_Of_The_World/CaptivePortal.sh
+chmod +x /opt/IT_For_The_End_Of_The_World/Web.sh
+chmod +x /opt/IT_For_The_End_Of_The_World/Music.sh
+
+###Captive Portal Service Install###
+echo '[Unit]
+Description=Captive Portal
+After=network.target
+
+[Service]
+ExecStart=/opt/IT_For_The_End_Of_The_World/CaptivePortal.sh
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=default.target' > /etc/systemd/system/CaptivePortal.service
+
+systemctl enable CaptivePortal.service
+systemctl start CaptivePortal.service
+
+###Web Service Install###
+echo '[Unit]
+Description=Web
+After=network.target
+
+[Service]
+ExecStart=/opt/IT_For_The_End_Of_The_World/Web.sh
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=default.target' > /etc/systemd/system/Web.service
+
+# Step 3: Enable and start the service
+systemctl enable Web.service
+systemctl start Web.service
+
+###Music Service Install###
+echo '[Unit]
+Description=Music
+After=network.target
+
+[Service]
+ExecStart=/opt/IT_For_The_End_Of_The_World/Music.sh
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=default.target' > /etc/systemd/system/Music.service
+
+# Step 3: Enable and start the service
+systemctl enable Music.service
+systemctl start Music
+
+.service
